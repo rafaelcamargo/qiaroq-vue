4fa4ecc2ca0c6c9e71776f34e3f04f0b7092fd89
Rafael Camargo <hello@rafaelcamargo.com>
2022-04-24 10:07:07 -0300

    chore: prerender setup

--- webpack.conf.prod.js
+++ webpack.conf.prod.js
@@ -1,4 +1,8 @@
 const TerserPlugin = require('terser-webpack-plugin');
+const PrerenderSPAPlugin = require('prerender-spa-plugin-next');
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
+const prerenderConfig = require('./webpack.conf.prerender');
+const project = require('./project.json');
 const webpack = require('webpack');
 
 module.exports = {
@@ -17,5 +21,23 @@ module.exports = {
         }
       })
     ]
-  }
+  },
+  plugins: [
+    new PrerenderSPAPlugin({
+      staticDir: `${__dirname}/${project.dist.root}`,
+      routes: prerenderConfig.getRoutes(),
+      minify: {
+        collapseBooleanAttributes: true,
+        collapseWhitespace: true,
+        decodeEntities: true,
+        keepClosingSlash: true,
+        sortAttributes: true
+      },
+      renderer: require('@prerenderer/renderer-puppeteer'),
+      rendererOptions: {
+        headless: true,
+        args: ['–no-sandbox', '–disable-setuid-sandbox']
+      }
+    })
+  ]
 }
